f429bec46372b480ae81665a527104daddd9490f
Vadim Alperovich <vadikalperovich@mail.ru>
2021-10-05 23:22:54 +0300

    added test results for EASY and MEDIUM :fire:

--- run_test.py
+++ run_test.py
@@ -1,9 +1,44 @@
 from main import *
 
-method_dct = {False: 'LP', True: 'ILP'}
+EASY = {
+    'benchmarks/DIMACS_all_ascii/johnson8-2-4.clq': 4,
+    'benchmarks/DIMACS_all_ascii/johnson16-2-4.clq': 8,
+    'benchmarks/DIMACS_all_ascii/MANN_a9.clq': 16,
+    'benchmarks/DIMACS_all_ascii/keller4.clq': 11,
+    'benchmarks/DIMACS_all_ascii/hamming8-4.clq': 16,
+}
+
+MEDIUM = {
+    'benchmarks/DIMACS_all_ascii/C125.9.clq': 34,
+    'benchmarks/DIMACS_all_ascii/gen200_p0.9_44.clq': 44,
+    'benchmarks/DIMACS_all_ascii/gen200_p0.9_55.clq': 55,
+    'benchmarks/DIMACS_all_ascii/brock200_1.clq': 21,
+    'benchmarks/DIMACS_all_ascii/brock200_2.clq': 12,
+    'benchmarks/DIMACS_all_ascii/brock200_3.clq': 15,
+    'benchmarks/DIMACS_all_ascii/brock200_4.clq': 17,
+    # 'benchmarks/DIMACS_all_ascii/p_hat1000-1.clq': 10,
+    # 'benchmarks/DIMACS_all_ascii/san1000.clq': 15,
+}
+
+HARD = {
+    'benchmarks/DIMACS_all_ascii/brock400_1.clq': 27,
+    'benchmarks/DIMACS_all_ascii/brock400_2.clq': 29,
+    'benchmarks/DIMACS_all_ascii/brock400_3.clq': 31,
+    'benchmarks/DIMACS_all_ascii/brock400_4.clq': 33,
+    'benchmarks/DIMACS_all_ascii/MANN_a27.clq': 126,
+    'benchmarks/DIMACS_all_ascii/MANN_a45.clq': 345,
+    'benchmarks/DIMACS_all_ascii/sanr400_0.7.clq': 21,
+    'benchmarks/DIMACS_all_ascii/sanr400_0.9.clq': 42,
+    'benchmarks/DIMACS_all_ascii/p_hat1000-2.clq': 46,
+    'benchmarks/DIMACS_all_ascii/p_hat500-3.clq': 50,
+    'benchmarks/DIMACS_all_ascii/p_hat1500-1.clq': 12,
+    'benchmarks/DIMACS_all_ascii/p_hat300-3.clq': 36,
+}
 
 
 def run_tests(benchmarks: list):
+    total_time = 0
+    method_dct = {False: 'LP', True: 'ILP'}
     for filepath in benchmarks:
         try:
             graph = utils.read_graph_file(filepath, verbose=False)
@@ -24,15 +59,15 @@ def run_tests(benchmarks: list):
             print(f'Found max clique size (obj func value): {obj_value}')
             print(*[f'x{i}={value:.2f}' for i, value in enumerate(solution) if value != 0])
             mean_time = np.mean(time_lst)
+            total_time += mean_time
             mins, secs = divmod(mean_time, 60)
             print(f'Execution time: {mins:.0f}min {secs:.1f}sec')
-            print(f'for {graph.number_of_nodes()} nodes and {graph.number_of_edges()} edges\n')
+            print(f'for {graph.number_of_nodes()} nodes and {graph.number_of_edges()} edges')
+            print(f'And clique of size {benchmarks[filepath]}!\n')
+
+    mins, secs = divmod(total_time, 60)
+    print(f"\n\n*****Total time is {mins:.1f}min {secs:.1f}sec ******")
 
 
 if __name__ == '__main__':
-    benches = ['benchmarks/DIMACS_all_ascii/hamming6-4.clq',
-               'benchmarks/DIMACS_all_ascii/C125.9.clq',
-               'benchmarks/DIMACS_all_ascii/brock200_1.clq',
-               'benchmarks/DIMACS_all_ascii/brock200_2.clq'
-               ]
-    run_tests(benches)
+    run_tests(MEDIUM)
